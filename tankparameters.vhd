10e80002b0343b41daa4a46755bd0559610bf712
Varshini Jeevaraj <32648876+varshj@users.noreply.github.com>
2017-11-16 17:01:15 -0600

    Add files via upload

---
+++ tankparameters.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+package tankparameters is
+
+constant  bullet_speed : integer := 5;
+constant tank_height : integer := 30;
+constant tank_width : integer := 60;
+constant bullet_height : integer := 5;
+constant bullet_width : integer := 5;
+
+end package;
+
+
+
+package body tankparameters is
+
+end package body;
